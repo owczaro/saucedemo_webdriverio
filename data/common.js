40477a82734893cd0cdf68ab4f762853726f92ea
owczaro <owczar.o@gmail.com>
2021-02-09 20:34:22 +0100

    Add data constants

---
+++ data/common.js
@@ -0,0 +1,3 @@
+export const wrongCredentialsErrorMessage =
+    'Epic sadface: Username and password do not match any user in this service';
+export const productName = 'Sauce Labs Backpack';
---
+++ data/credentials.js
@@ -0,0 +1,9 @@
+export const validCredentials = {
+    username: 'standard_user',
+    password: 'secret_sauce',
+};
+
+export const invalidCredentials = {
+    username: 'incorrect_user',
+    password: 'incorrect_password',
+};
